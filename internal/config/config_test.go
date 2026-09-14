bb8ef46b25685f51d8e691416ee0c8416fbacc57
NOMAD\25090 <2509094405@qq.com>
2026-09-14 22:23:47 +0800

    test: 修复过期时间段匹配不上的问题，通过CI测试

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -3,13 +3,13 @@ package config
 import "testing"
 
 func TestJWTConfigValidateRejectsShortSecret(t *testing.T) {
-	if err := (JWTConfig{Secret: "short", ExpiresIn: 86400}).Validate(); err == nil {
+	if err := (JWTConfig{Secret: "short", ExpireHours: 24}).Validate(); err == nil {
 		t.Fatal("expected short JWT secret to be rejected")
 	}
 }
 
 func TestJWTConfigValidateAcceptsUsableConfig(t *testing.T) {
-	if err := (JWTConfig{Secret: "01234567890123456789012345678901", ExpiresIn: 86400}).Validate(); err != nil {
+	if err := (JWTConfig{Secret: "01234567890123456789012345678901", ExpireHours: 24}).Validate(); err != nil {
 		t.Fatalf("expected config to be valid: %v", err)
 	}
 }
